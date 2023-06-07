a7a4ace86e67193d18b3cb5066b86ab5c2603aa2
Shubham Kadam <shubhamkadam@Shubhams-MacBook-Pro.local>
2023-06-07 19:33:35 +0530

    Reversing an integer

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,11 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  let newNum = n.toString().split("").reverse().join("");
+  newNum = parseInt(newNum);
 
-module.exports = reverseInt;
+  return newNum * Math.sign(n);
+}
+
+console.log(reverseInt(-120));
